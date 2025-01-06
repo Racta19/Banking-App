e378bd1064a146226f62be609608324371f548ee
Muhammad Rohail Omer <77564698+Racta19@users.noreply.github.com>
2025-01-06 00:50:20 +0000

    Layout implement(completed)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Rohail',lastName:'Omer',emai:'u@mail.com'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type='greeting' title='Welcome' user={loggedIn?.firstName || 'Guest'} subtext='Access and manage your account and transections effectively.' />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+        Recent transactions
+      </div>
+      
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+    </section>
   )
 }
 
